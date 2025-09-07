afe5d70aec140a6b33391232268eb599f9d1a7b9
Godsplan Elot <elotgodsplan2@gmail.com>
2025-09-07 14:13:49 +0100

    Finish grid 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import About from './sections/About'
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
-      {/* about */}
+      <About />
+      {/* <section className='min-h-screen' /> */}
       {/* projects */}
       {/* experience */}
       {/* testimonial */}
