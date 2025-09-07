a5daa4e7536073c55b05d24da2dea48f50f120e8
Godsplan Elot <elotgodsplan2@gmail.com>
2025-09-07 19:31:01 +0100

    Finish about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/Globe";
 import CopyEmailButton from "../components/CopyEmailButton";
+import { Frameworks } from "../components/Frameworks";
 
 const About = () => {
     const grid2Container = useRef();
@@ -60,7 +61,14 @@ const About = () => {
         </div>
         {/* Grid 5 */}
         <div className="grid-default-color grid-5" >
+          <div className="z-10 w-[50%]" >
+            <p className="headText" >Tech Stack</p>
+            <p className="subtext">I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable applications</p>
+          </div>
 
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+            <Frameworks />
+          </div>
         </div>
       </div>
     </section>
