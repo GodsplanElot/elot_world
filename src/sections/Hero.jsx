c92a479f5d21e5d8b568ebe799d1bdcbe146ad10
GodsplanElot <elotgodsplan2@gmail.com>
2026-05-11 01:52:51 +0100

    feat: replace Astronaut with LightningBolt in Hero section and add lighting effects

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ import { Float} from "@react-three/drei"
 import { useFrame } from "@react-three/fiber"
 import { easing } from "maath"
 import { useMediaQuery } from "react-responsive"
-import { Astronaut } from "../components/Astronaut"
+import { LightningBolt } from "../components/LightningBolt"
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
 import { Suspense } from "react"
@@ -17,11 +17,14 @@ const Hero = () => {
       <ParallaxBackground />
       <figure className="absolute inset-0" style={{width: "100vw", height: "100vh"}}>
         <Canvas camera={{ position: [0, 1, 3] }} dpr={[1, 1.5]} gl={{ antialias: false }}>
+          <ambientLight intensity={0.45} />
+          <pointLight position={[2, 3, 3]} intensity={1.2} color="#ffd966" />
+          <directionalLight position={[-2, 1, 1]} intensity={0.6} color="#ffe6a0" />
           <Suspense fallback={<Loader />}>
-            <Float>
-              <Astronaut
-                scale={isMobile && 0.23}
-                position={isMobile && [0, -1.5, 0]}
+            <Float floatIntensity={1.2} rotationIntensity={0.3} speed={1.2}>
+              <LightningBolt
+                scale={isMobile ? 1.1 : 1.6}
+                position={isMobile ? [0, -0.2, 0] : [0, -0.15, 0]}
               />
             </Float>
             <Rig />
