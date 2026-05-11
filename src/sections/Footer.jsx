fade1ac157537c877e263b8fd32a25c26150e8e6
GodsplanElot <elotgodsplan2@gmail.com>
2026-05-11 01:06:50 +0100

    feat: add Footer component for site navigation and policy links

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,17 @@
+import { mySocials } from "../constants"
+
+const Footer = () => {
+  return <section className="flex  flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <div className='mb-4 bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full' />
+    <div>
+        <p>Terms & Conditions</p>
+        <p>Privacy Policy</p>
+        <p></p>
+    </div>
+    <div className="flex gap-3">
+
+    </div>
+  </section>
+}
+
+export default Footer
